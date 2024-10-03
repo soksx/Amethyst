072bb9e9865df5bbcdef88a91dd8620901fdf42c
Iván del Cura Nieto <ivandelcura@hotmail.com>
2024-10-03 14:17:23 +0000

    - chore(sigs): Update sigs for Minecraft Bedrock `1.21.31.4`

--- AmethystRuntime/src/hooks/Hooks.cpp
+++ AmethystRuntime/src/hooks/Hooks.cpp
@@ -82,8 +82,8 @@ void LevelEvent(Level* level) {
     AmethystRuntime::getEventBus()->Invoke(event);
 }
 
-void* ClientInstance__ClientInstance(ClientInstance* self, uint64_t a2, uint64_t a3, uint64_t a4, char a5, void* a6, void* a7, uint64_t a8, void* a9) {
-    void* ret = _ClientInstance__ClientInstance.call<void*>(self, a2, a3, a4, a5, a6, a7, a8, a9);
+void* ClientInstance__ClientInstance(ClientInstance* self, uint64_t a2, uint64_t a3, void* a4, uint64_t a5, void* a6, uint64_t a7, uint64_t a8) {
+    void* ret = _ClientInstance__ClientInstance.call<void*>(self, a2, a3, a4, a5, a6, a7, a8);
 
     AmethystRuntime::getContext()->mClientInstance = self;
 
@@ -109,8 +109,8 @@ Minecraft* Minecraft__Minecraft(Minecraft* a1, void* a2, void* a3, void* a4, voi
     return a1;
 }
 
-void BlockGraphics_initBlocks(ResourcePackManager& resources, const Experiments& experiments) {
-    _BlockGraphics_initBlocks.call<void, ResourcePackManager&, const Experiments&>(resources, experiments);
+void BlockGraphics_initBlocks(ResourcePackManager& resources, const Experiments& experiments, void* a3) {
+    _BlockGraphics_initBlocks.call<void, ResourcePackManager&, const Experiments&, void*>(resources, experiments, a3);
 
     InitBlockGraphicsEvent event(resources, experiments);
     AmethystRuntime::getEventBus()->Invoke(event);
@@ -131,11 +131,11 @@ void CreateModFunctionHooks() {
 
     NO_THROW_HOOK(ScreenView, setupAndRender, "48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B FA");
     NO_THROW_HOOK(Minecraft, update, "48 8B C4 48 89 58 ? 48 89 70 ? 48 89 78 ? 55 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B E9 48 89 4C 24");
-    NO_THROW_HOOK(ClientInstance, onStartJoinGame, "40 55 53 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 45 8B F1");
+    NO_THROW_HOOK(ClientInstance, onStartJoinGame, "40 55 53 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 41 8B D9");
     NO_THROW_HOOK(ClientInstance, requestLeaveGame, "48 89 5C 24 ? 48 89 74 24 ? 55 57 41 54 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 45 0F B6 F0 44 0F B6 FA");
-    NO_THROW_HOOK(ClientInstance, _ClientInstance, "48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 49 8B F9 49 8B D8 4C 8B E2");
+    NO_THROW_HOOK(ClientInstance, _ClientInstance, "48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 49 8B F9 49 8B D8 48 89 5C 24");
     NO_THROW_HOOK(VanillaItems, registerItems, "40 55 53 56 57 41 54 41 56 41 57 48 8D AC 24 ? ? ? ? B8 ? ? ? ? E8 ? ? ? ? 48 2B E0 0F 29 B4 24");
-    NO_THROW_HOOK(BlockDefinitionGroup, registerBlocks, "48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 41 54 41 55 41 56 41 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 45 33 E4");
-    NO_THROW_HOOK(BlockGraphics, initBlocks, "48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B E2 48 89 95 ? ? ? ? 4C 8B F9 48 89 4D");
-    NO_THROW_HOOK(Minecraft, _Minecraft, "48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 4D 8B E1 49 8B D8 4C 8B EA");
+    NO_THROW_HOOK(BlockDefinitionGroup, registerBlocks, "48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 48 8B EA 48 81 C2");
+    NO_THROW_HOOK(BlockGraphics, initBlocks, "48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 4D 8B E0 48 89 54 24 ? 48 8B F1");
+    NO_THROW_HOOK(Minecraft, _Minecraft, "48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 4D 8B F1 49 8B D8");
 }
